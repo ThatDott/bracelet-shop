541d7781f424cdc4d6d12a9168c054f798c315ec
Justin <justinsanveloso@gmail.com>
2025-01-15 05:50:32 +0800

    feat: improved individual section layouts

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ window.addEventListener('scroll', function() {
     nav.classList.remove('scrolled');
   }
 });
+
+// Slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function moveSlide(n) {
+  showSlides(slideIndex += n)
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.remove("active");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
