88c8409ec9c7c806cf8d34ae57496936055abcd4
Justin <justinsanveloso@gmail.com>
2025-01-17 05:20:13 +0800

    feat: added cross-functionality for sort and filter functions in products page

--- product-list.js
+++ product-list.js
@@ -1,6 +1,31 @@
+let currentSortId = "name";
+let currentFilterId = 0;
+
+// Set Active Button
+function setActiveButton(e, buttonClass) {
+  document.querySelectorAll(buttonClass).forEach(button => button.classList.remove("active"));
+  e.currentTarget.classList.add("active");
+}
+
+// Set Sort Id
+function sortProducts(e, newSortId) {
+  setActiveButton(e, ".sort-button");
+  currentSortId = newSortId;
+  loadProducts(currentSortId, currentFilterId);
+}
+
+// Set Filter Id
+function filterProducts(e, newFilterId) {
+  setActiveButton(e, ".filter-button");
+  currentFilterId = newFilterId;
+  loadProducts(currentSortId, currentFilterId);
+}
+
+// Load Products
 function loadProducts(sortId, filterId) {
   const productList = document.querySelector(".product-list");
   const emptyMessage = document.createElement("h2");
+
   // Message If No Products
   emptyMessage.textContent = "No Items Found";
 
@@ -13,24 +38,31 @@ function loadProducts(sortId, filterId) {
       return res.json();
     })
     .then((products) => {
-      let productsArray = Object.keys(products).map(key => products[key]);
-      
+      let productsArray = Object.keys(products).map((key) => products[key]);
+
       // Filter Products
       if (filterId != 0) {
         productsArray = productsArray.filter((product) => {
-          return (product.category == filterId)
-        })
+          return product.category == filterId;
+        });
       }
 
       // Sort Products
-      if (sortId != 0) {
+      if (sortId == "price") {
+        productsArray.sort((a, b) => {
+          // Removing "P" and parsing price for numeric comparison
+          const priceA = parseFloat(a.price.replace("P", "").replace(",", "").trim());
+          const priceB = parseFloat( b.price.replace("P", "").replace(",", "").trim());
+          return priceA - priceB;
+        });
+      } else if (sortId != 0) {
         productsArray.sort((a, b) => a[sortId].localeCompare(b[sortId]));
       }
       // Reset Products
       productList.innerHTML = "";
 
       // Generate Products in DOM
-      productsArray.forEach(product => {
+      productsArray.forEach((product) => {
         if (product) {
           const newProduct = document.createElement("div");
           const name = document.createElement("h3");
@@ -50,11 +82,12 @@ function loadProducts(sortId, filterId) {
         } else {
           productList.appendChild(emptyMessage);
         }
-      })
+      });
     })
     .catch((error) => console.error("Error: ", error));
 }
 
+// Initialization
 document.addEventListener("DOMContentLoaded", function () {
-  loadProducts("name", 0);
+  loadProducts(currentSortId, currentFilterId);
 });
