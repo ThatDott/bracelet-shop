5162c1ce6a8d5317ad97bc8549454f4206cba6cc
Justin <justinsanveloso@gmail.com>
2025-01-26 06:41:47 +0800

    fix: debugged slider in index.js

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 // Slider
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+
+document.addEventListener("DOMContentLoaded", function () {
+  showSlides(slideIndex);
+});
 
 function moveSlide(n) {
-  showSlides(slideIndex += n)
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n)
+  showSlides((slideIndex += n));
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n >= slides.length) { slideIndex = 0 }
+  if (n < 0) { slideIndex = slides.length - 1 }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.remove("active");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  console.log(slides[slideIndex]);
+  slides[slideIndex].style.display = "block";
 }
