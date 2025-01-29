5e5784759b01a9213ff0644dfc43cf9026b923ea
Justin <justinsanveloso@gmail.com>
2025-01-29 12:32:19 +0800

    feat: added add-to-cart function

--- product-details.js
+++ product-details.js
@@ -3,13 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(urlString);
   const urlId = urlParams.get("id");
 
-  // Fetch JSON file
+  // Fetch JSON file and transform into Product Array
   function fetchProducts(callback) {
     const file = "products.json";
     fetch(file)
       .then((res) => {
         if (!res.ok) {
-          console.error("Failed to fetch JSON: ", error);
+          throw new error("Failed to fetch JSON: ", error);
         }
         return res.json();
       })
@@ -18,20 +18,24 @@ document.addEventListener("DOMContentLoaded", () => {
           ...products[key],
           index: key,
         }));
-        callback(productsArray);
+        
+        // Get product with matching Id
+        const product = productsArray.find((prod) => prod.index === urlId);
+        callback(product);
       })
       .catch((error) => console.error("Error: ", error));
   }
 
-  function loadProduct(productsArray) {
+  // Load Product Info into DOM
+  function loadProduct(product) {
+    // Find existing elements in DOM
     const name = document.getElementById("product-name");
     const category = document.getElementById("product-category");
     const price = document.getElementById("product-price");
     const description = document.getElementById("product-description");
-    const image = document.getElementById("product-picture");
+    const image = document.getElementById("product-image");
 
-    const product = productsArray.find((prod) => prod.index === urlId);
-
+    // Replace DOM elements' content based on JSON file
     if(product) {
       name.innerHTML = product.name;
       category.innerHTML = product.category;
@@ -43,9 +47,38 @@ document.addEventListener("DOMContentLoaded", () => {
       category.innerHTML = "";
       price.innerHTML = "";
       description.innerHTML = "";
-      image.src = product.image;
-
+      image.src = "";
     }
   }
-  fetchProducts(loadProduct);
+  
+  // Add to Cart Button Function
+  function addToCart(product) {
+    if (product) {
+      // Get Saved Cart Count or default to 0
+      let cartCount = localStorage.getItem("cartCount");
+      cartCount = cartCount ? parseInt(cartCount) : 0;
+      
+      // Increment and Store New Count
+      cartCount++;
+      localStorage.setItem("cartCount", cartCount);
+      alert(`Added '${product.name}' to shopping cart.\nTotal Items: ${cartCount}`);
+    } else {
+      alert("Product not found. Cannot add to cart.");
+    }
+  }
+
+  // Fetch Product and Set Up Button listener
+  fetchProducts((product) => {
+    loadProduct(product);
+
+    const addToCartButton = document.getElementById("add-to-cart");
+    const buyNowButton = document.getElementById("buy-now");
+    if (addToCartButton && buyNowButton) {
+      addToCartButton.addEventListener("click", () => addToCart(product));
+      buyNowButton.addEventListener("click", () => {
+        addToCart(product);
+        window.location.href = "./shopping-cart.html";
+      });
+    }
+  });
 });
